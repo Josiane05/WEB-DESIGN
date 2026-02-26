4e697854019c8c838bc76a8af16e4d5ff85e72e1
Josiane05 <josianemushimiyimana05@gmail.com>
2026-02-26 10:31:11 +0200

    calculating monthly salary and annual salary

--- File26.js
+++ File26.js
@@ -1,8 +1,19 @@
 function Monthly(){
-let Gross=document.getElementById('gs').value;
-let Transport=document.getElementById('ta').value;
-let Insuarance=document.getElementById('hi').value;
-let Tax=document.getElementById('tx').value;
+let Gross=parseInt(document.getElementById('gs').value);
+let Transport=parseInt(document.getElementById('ta').value);
+let Insuarance=parseInt(document.getElementById('hi').value);
+let Tax=parseInt(document.getElementById('tx').value);
 let MSalary=Gross+Transport-Insuarance-Tax;
 document.write(MSalary);
+}
+
+function Annual(){
+let Gross=parseInt(document.getElementById('gs').value);
+let Transport=parseInt(document.getElementById('ta').value);
+let Insuarance=parseInt(document.getElementById('hi').value);
+let Tax=parseInt(document.getElementById('tx').value);
+let MSalary=Gross+Transport-Insuarance-Tax;
+let ASalary=MSalary*12;
+document.write(ASalary);
+
 }
